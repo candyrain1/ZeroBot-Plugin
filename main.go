086b98c9eaf52c792399150b85a9ba3b130c7438
candyrain1 <109255263+candyrain1@users.noreply.github.com>
2022-07-14 12:51:41 +0800

    zhuren

--- main.go
+++ main.go
@@ -200,8 +200,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 1508921569)
-	// sus = append(sus, 87654321)
+	sus = append(sus, 1508921569)
+	
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
